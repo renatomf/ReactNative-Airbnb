1bd19bd396b4196db75003dbd9eb694fe01a7a7d
red rat <renatomar76@gmail.com>
2019-10-21 16:57:43 -0300

    Forgot Password Page

--- App.js
+++ App.js
@@ -6,12 +6,13 @@ import {
 } from 'react-native';
 
 //import LoggedOut from './src/screens/LoggedOut';
-import LogIn from './src/screens/LogIn';
+//import LogIn from './src/screens/LogIn';
+import ForgotPassword from './src/screens/ForgotPassword';
 
 
 export default class App extends Component {
   render() {
-    return <LogIn />
+    return <ForgotPassword />
   }
 }
 
